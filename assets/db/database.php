d62ed5b5444971a970a718e2b19e9fa825090596
Alessandro Zirondelli <alessandr.zirondelli@studio.unibo.it>
2021-12-28 15:27:54 +0100

    move database.php to assets/db

--- assets/db/database.php
+++ assets/db/database.php
@@ -1,17 +1,24 @@
 <?php 
+//mysqli_close($conn);
+
 class DatabaseHelper{
-    private $db;
+    private $conn;
 
-    public function __construct($servername,$username,$password,$dbname,$port){
-        //connessione al DB
-        $this->db = new mysqli($servername,$username,$password,$dbname,$port);
-        if($this->db->connect_error){
-            die("Connection failed");
-        }
+    public function __construct($servername, $username, $password, $dbname, $port){
+        $this->conn = new mysqli($servername, $username, $password, $dbname, $port);
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }        
     }
-
-    //Qui verranno inserite tutte le query e le insert di cui abbiamo bisogno
-
-
 }
+
+class Manager{
+    private $dbh;
+
+    public function __construct() {
+        $this->dbh = new DatabaseHelper("localhost","root","", "drinkdb",3306);
+    }
+}
+
 ?>
+
