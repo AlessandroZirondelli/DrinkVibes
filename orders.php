5eafed8a603e826d45076968eb3958425c418814
Alessandro Zirondelli <alessandr.zirondelli@studio.unibo.it>
2021-12-28 15:40:51 +0100

    Adjust base style css. Add main-orders.php

--- orders.php
+++ orders.php
@@ -2,7 +2,7 @@
     /*questo file di boostrap si occupa do sistmare la connesione al database  */
     require_once("bootstrap.php"); //require once, se è gia incliso non lo reinclude. C'è anche require e basta.
     $templateParams["title"] = "Home";
-    $templateParams["main-content"] = "";
+    $templateParams["main-content"] = "main-orders.php";
     $cssArray[0]="./assets/css/orders-style.css";
     require_once("template/base.php"); //base.php vede la roba definita dentro  e dentro questo file index.php
 ?>
