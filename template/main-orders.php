302c8d0173abb79d11790f05c6411025de20b910
Alessandro Zirondelli <alessandr.zirondelli@studio.unibo.it>
2021-12-28 20:56:43 +0100

    Test container for orders

--- template/main-orders.php
+++ template/main-orders.php
@@ -1,6 +1,59 @@
 <?php
 echo '
+<div class="container">
+    <div class="row">
+        <div class="col-md-2 pt-5">
+        </div>
 
+        <div class="col-md-8 pt-5">
+            <h2 class="h2 text-black ">Orders</h2>
+            <ul class="nav nav-tabs" id="myTab" role="tablist">
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link active" id="all-orders-tab" data-bs-toggle="tab" data-bs-target="#allorders" type="button" role="tab" aria-controls="home" aria-selected="true">Home</button>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link" id="pending-orders-tab" data-bs-toggle="tab" data-bs-target="#pendingorders" type="button" role="tab" aria-controls="profile" aria-selected="false">Profile</button>
+                </li>
+                
+            </ul>
+            <div class="tab-content" id="myTabContent">
+                <div class="tab-pane fade show active" id="allorders" role="tabpanel" aria-labelledby="all-orders-tab"> 
+                    <div class="container">
+                        <div>
+                            ciaoaoao
+                        </div>
+                        <div>
+                            bellissimi
+                        </div>
+                        <div>
+                            bellissimi
+                        </div>
+                        
+                    </div>
+
+                    <div class="container">
+                        <div>
+                            ggghch
+                        </div>
+                        <div>
+                            belnhkbklissimi
+                        </div>
+                        <div>
+                            hffyxfyi
+                        </div>
+                        
+                    </div>
+    
+                </div>
+                <div class="tab-pane fade" id="pendingorders" role="tabpanel" aria-labelledby="pending-orders-tab">  bb</div>
+
+            </div>
+        </div>
+
+        <div class="col-md-8 pt-5">
+        </div>
+    </div>
+</div>
 '
 
 ?>
