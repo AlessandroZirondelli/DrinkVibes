410381bc6d64365988ee9c345dd16abf0801ffcf
Lorenzo Sansone <lorenzo.sansone3@studio.unibo.it>
2022-01-20 21:43:11 +0100

    add files

--- makeyourdrink.php
+++ makeyourdrink.php
@@ -3,7 +3,7 @@
     require_once("bootstrap.php"); //require once, se è gia incliso non lo reinclude. C'è anche require e basta.
     $templateParams["title"] = "Make your drink";
     $templateParams["main-content"] = "main-makeyourdrink.php";
-    $cssArray[0]="./assets/css/index-style.css";
+    $cssArray[0]="./assets/css/makeyourdrink-style.css";
     require_once("template/base.php"); //base.php vede la roba definita dentro  e dentro questo file index.php
 ?>
 
