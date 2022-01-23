2fcd855a7c4300ff63a8681e8e766244e2e84e6a
Alessandro Zirondelli <alessandr.zirondelli@studio.unibo.it>
2022-01-23 23:54:16 +0100

    Add accordion button for more info and test

--- template/main-orders.php
+++ template/main-orders.php
@@ -16,6 +16,45 @@
             <div class="tab-content" id="myTabContent">
                 <div class="tab-pane fade show active" id="allorders" role="tabpanel" aria-labelledby="all-orders-tab"> 
                     <div class="container">
+                        
+                    <div class="accordion accordion-flush" id="accordionFlushExample">
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="flush-headingOne">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                                    More info
+                                </button>
+                            </h2>
+                            <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                                <div class="accordion-body">
+                                    Text 
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                                            
+                    <!-- 
+
+
+
+                    <div class="container">
+    <div class="row">
+        <div class="col-md-2">
+        </div>
+
+        <div class="col-md-8 pt-5">
+            <h2 class="h2 text-black ">Orders</h2>
+            <ul class="nav nav-tabs" id="myTab" role="tablist">
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link active" id="all-orders-tab" data-bs-toggle="tab" data-bs-target="#allorders" type="button" role="tab" aria-controls="home" aria-selected="true">All orders</button>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link" id="pending-orders-tab" data-bs-toggle="tab" data-bs-target="#pendingorders" type="button" role="tab" aria-controls="profile" aria-selected="false">Orders being processed</button>
+                </li>   
+            </ul>
+            <div class="tab-content" id="myTabContent">
+                <div class="tab-pane fade show active" id="allorders" role="tabpanel" aria-labelledby="all-orders-tab"> 
+                    <div class="container">
                         <div class="row">
                             <div class="col-4 col-md-4 p-0">
                                 <div>
@@ -42,17 +81,35 @@
                                 </div>
                             </div>
                         </div>
-                        <div>
-                            1 Coca-Cola
+
+                        <div class="row"> 
+                            <div>
+                                1 Coca-Cola
+                            </div>
+                            <div>
+                                1 Mojito
+                            </div>
                         </div>
-                        <div>
-                            1 Mojito
-                        </div>
+                        
                         
                     </div>
 
                    
     
+                </div>
+                <div class="tab-pane fade" id="pendingorders" role="tabpanel" aria-labelledby="pending-orders-tab">  bb</div>
+
+            </div>
+        </div>
+
+        <div class="col-md-2">
+        </div>
+    </div>
+</div>
+                    -->                      
+
+
+                    </div>
                 </div>
                 <div class="tab-pane fade" id="pendingorders" role="tabpanel" aria-labelledby="pending-orders-tab">  bb</div>
 
