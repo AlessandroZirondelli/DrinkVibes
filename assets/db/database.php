3b6d571a78c2eef0a0f87afa49a22b3f62e5c47d
Alessandro Zirondelli <alessandr.zirondelli@studio.unibo.it>
2022-01-30 23:05:30 +0100

    Add query . Add subtotal field in ordersdetails.

--- assets/db/database.php
+++ assets/db/database.php
@@ -12,47 +12,78 @@ class DatabaseHelper{
         echo "Connessione OK" ;      
     }
 
-    /* Devo fare una funzione che mi faccia un query e mi restituisca tutti gli ordini di un utente */
+    /* FETCH ALL Devo fare una funzione che mi faccia un query e mi restituisca tutti gli ordini di un utente */
     public function getOrdersByUser($userID){ //extract elements that belongs to specified set
         $stmt = $this->conn->prepare("SELECT * FROM totalorders WHERE userID=?");
         $stmt-> bind_param("s",$userID);
         $stmt->execute();
         $result = $stmt->get_result();
-        echo "Query fatta";
+        //echo "Query fatta";
         return $result->fetch_all(MYSQLI_ASSOC);
     }
-}
-
-class Manager{
-    private $dbh;
-
-    public function __construct() {
-        $this->dbh = new DatabaseHelper("localhost","root","", "drinkdb",3306);
+    /*FETCH ALL.Questa funzione restituisce tutti i dettagli ordine relativi allo specifico ordine */
+    public function getOrderDetails($orderID){
+        $stmt = $this->conn->prepare("SELECT * FROM orderdetails WHERE orderID=?");
+        $stmt-> bind_param("i",$orderID);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        echo "Query order details fatta";
+        return $result->fetch_all(MYSQLI_ASSOC);
     }
+    /* DATO ELBAORATO. Restituisce il nome di un prodotto, dato il suo ID. Oppure Custom drink. */ 
+    public function getArticleName($articleID){
+        $stmt = $this->conn->prepare("SELECT name FROM product  WHERE productID=?");
+        $stmt-> bind_param("i",$articleID);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        echo "Query article name details fatta";
+        $res = $result->fetch_all(MYSQLI_ASSOC);
+        if (!$res){ //se non ho trovato alcuna corrispondenza allora cerco nella tabella drinkhandmade 
+            return "Custom drink";
+        }
+        return $res[0]["name"];
+    }
+
+    public function getSubtotalPrice($orderID,$articleID){ 
+        $stmt = $this->conn->prepare("SELECT subtotal FROM orderdetails  WHERE orderID=? AND articID=?");
+        $stmt-> bind_param("ii",$orderID,$articleID);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        echo "Query article price fatta";
+        $res = $result->fetch_all(MYSQLI_ASSOC);
+        return $res[0]["subtotal"];
+    }
+
+
 
 
     /*
-    getALlOrders() 
-    Questa funzione dovrà restituire lo storico di  tutti gli ordini che il cliente ha effettuato 
-    */
+    private function getUnitIngredientPrice(){
+        $stmt = $this->conn->prepare("SELECT price FROM unitingredient  WHERE productID=?");
+        $stmt-> bind_param("i",$articleID);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $res = $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    private function getLiquidIngredientPrice(){
+        $stmt = $this->conn->prepare("SELECT price FROM liquidingredient  WHERE productID=?");
+        $stmt-> bind_param("i",$articleID);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $res = $result->fetch_all(MYSQLI_ASSOC);
+    }
+*/
+    /*DATO ELABORATO. Restituisce il subtotal */ 
+  
+    
 }
 
+
 $dbhelper = new DatabaseHelper("localhost","root","", "drinkdb",3306);
-$resQuery = $dbhelper->getOrdersByUser("Nick987");
-/*
-foreach ($resQuery as $tmp) {
-    $orderID = $tmp["orderID"];
-    echo "<br> $orderID"; 
-    $user = $tmp["userID"];
-    echo "<br> $user";
-    $date = $tmp["date"];
-    echo "<br> $date";
-    $time = $tmp["time"];
-    echo "<br> $time";
-    $state = $tmp["state"];
-    echo "<br> $state";
-}
-*/
+
+$resQuery = $dbhelper->getSubtotalPrice(1,2);
+echo "$resQuery";
 
 
 ?>
