870f9cfad926a3c99fa7f2cf6814f4a4e5c24a86
Alessandro Zirondelli <alessandr.zirondelli@studio.unibo.it>
2022-02-08 00:09:39 +0100

    Add refresh in admin page tabs when state is delivered

--- assets/js/dropdownStatus.js
+++ assets/js/dropdownStatus.js
@@ -13,7 +13,7 @@ function switchSelectedItem(){
             $(this).addClass("active");//evidenzio l'item appena selezionato
             
 
-            $orderID=$(this).parent().parent().attr('id').match(/\d+/);;
+            $orderID=$(this).parent().parent().attr('id').match(/\d+/);//serve per prendere solo la parte numerica dell'ID che corrisponde all'ID dell'ordine
             $state=$(this).text().replace(/\s+/g, '+');
             
             const xhttp = new XMLHttpRequest();
@@ -23,7 +23,66 @@ function switchSelectedItem(){
             }
             xhttp.open("GET", "utils/updateOrderState.php?id="+$orderID+"&state="+$state);
             xhttp.send();
+
+
+
+
+            //controllo se il tag div con class "dropdown" contiene tabOne nell'id o tabTwo
+            dropDownSelectedID= $(this).parent().parent();
+            
+            //$containerToDelete= $dropDownSelectedID.closest(".container").remove();
+
+            if($('#tab2').length!=0){ // controllo se sono amministratore
+                if($(this).text()=="Delivered"){ 
+                    //elimino solo il container dell'ordine seleziomato nel tab2
+                    
+                    containerToRemove= dropDownSelectedID.closest(".container");
+                    selectedTab=dropDownSelectedID.attr('id');
+                    number=selectedTab.match(/\d+/);
+                    state=$(this).text();
+                    console.log(state);
+                    if(selectedTab.includes("tabTwo")){ //se ho selezionato il secondo tab
+                        containerToRemove.remove();
+                        dropdownToChange="tabOne".concat(number);
+                        // devo aggiornare tab1
+                        $("#"+dropdownToChange).find(".active").removeClass("active");
+                        //devo attivare la classe a che ha text = state
+                        //console.log($('#'+dropdownToChange+);
+                        //$("#"+dropdownToChange+">li a:contains()")
+                        $('#'+dropdownToChange+' li:contains('+state+') a').addClass("active");
+                    }
+                    else{//selectedTab.includes("tabOne")) //se ho selezionato il primo tab     
+                        dropdownToChange="tabTwo".concat(number);  
+                        $("#"+dropdownToChange).closest(".container").remove();
+                        // NON devo aggiornare tab2, in quanto lo ho già rimoss
+                    }
+                    
+                    
+                }   
+            }
+            else{//se sono fattorino
+
+
+            }
+            
+                      
+
+
+
+            //if($dropDownSelectedID.includes("tabOne")){ //ho cliccato sul dropDown della tabella 1
+                //devo aggiornare il dropDownDella secondo tab
+                //se è presente un tabTwo ma con lo stesso ID finale allora aggiorna la selezione active
+
+            //}
+            
+            
             
         }
     );
+}
+
+
+function updateStateInSpecificTabs(selectedState,order,tabsNumber){
+    //$(this).parent().parent().find(".active").removeClass("active"); //prendo l'elemento che era selezionato prima e deseleziono
+    $(this).addClass("active");//evidenzio l'item appena selezionato
 }
