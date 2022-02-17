ad0b2bb8470104345e5d90eacc74f92ae33944d8
Alessandro Zirondelli <alessandr.zirondelli@studio.unibo.it>
2022-02-17 15:55:18 +0100

    clean code sendNotifications.php

--- utils/sendNotifications.php
+++ utils/sendNotifications.php
@@ -4,29 +4,20 @@ require_once("./../assets/db/database.php");
 $dbh = new DatabaseHelper("localhost", "root", "", "drinkdb",3306);
 
 $notifType= $_REQUEST["notifType"]; 
-//$notifType="stateChanged";
-/* 
-    notifType=stateChanged; //questa è la notifica relativa al cambio di stato 
-    notifType=orderReady; //questa è la notifica relativa ad un nuovo ordine prondo per spedire
-    notifType=newOrder; //questa è la notificarelativa ad un nuovo ordine effettuato
-*/
 
 
-if($notifType=="stateChanged"){ //se è una notifica di cambio di stato
-    $orderRef=$_REQUEST["orderRef"]; //prendo il riferimento all'ordine
+
+if($notifType=="stateChanged"){ 
+    $orderRef=$_REQUEST["orderRef"]; 
     $userRef = $dbh->getUserIDByOrderID($orderRef);
     $changedState = $dbh->getStateByOrderID($orderRef);
 
     $dbh->insertNotifOrderState($orderRef,$userRef,$changedState);
 
-    //Lo stato dell'ordine numero 5362762 è in stato : Ready to prepare "
-
-    //Quando vado a scaricare i dati per far vedere le notofioche:
-    //faccio il filtro delle notifiche con forWho:User con specifico userRef
 }
 elseif($notifType=="orderReady"){
     $orderRef=$_REQUEST["orderRef"];
-    $dbh->insertNotifOrderReady($orderRef,"Express"); //l'utente che deve leggere le notifiche è Express
+    $dbh->insertNotifOrderReady($orderRef,"Express"); // User taht must read notification is Express
 }
 elseif($notifType=="newOrder"){
     $orderRef=$_REQUEST["orderRef"];
@@ -40,10 +31,6 @@ elseif($notifType=="soldout"){
     $articleNameRef=$_REQUEST["articleNameRef"];
     $dbh->insertiNotifSoldout($articleIDRef,$articleNameRef);
 }
-
-/*$id = $_REQUEST["id"];
-$state = $_REQUEST["state"];
-*/
 
 
 
