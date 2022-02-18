88a5e1005b64974d585af57a9e203895f09ff111
Alessandro Zirondelli <alessandr.zirondelli@studio.unibo.it>
2022-02-18 17:07:22 +0100

    Add new icons for categories

--- template/main-index.php
+++ template/main-index.php
@@ -11,17 +11,17 @@
         <div class="row">
           <div class="col-12 col-md-4">
             <a href="#">
-              <img src="./assets/img/wine-icon.jpg" class="img-fluid rounded-circle" alt="Button image spirits" />
+              <img src="./assets/img/WineIcon.png" class="img-fluid rounded-circle" alt="Button image spirits" />
             </a>
           </div>
           <div class="col-12 col-md-4">
             <a href="#">
-              <img src="./assets/img/cocktail-icon.jpg" class="img-fluid rounded-circle" alt="Button image cocktail" />
+              <img src="./assets/img/BeveragesIcon.png" class="img-fluid rounded-circle" alt="Button image cocktail" />
             </a>
           </div>
           <div class="col-12 col-md-4">
             <a href="#">
-              <img src="./assets/img/spirits-icon.png" class="img-fluid rounded-circle" alt="Button image spirits" />
+              <img src="./assets/img/DrinksIcon.png" class="img-fluid rounded-circle" alt="Button image spirits" />
             </a>
           </div>
         </div>
