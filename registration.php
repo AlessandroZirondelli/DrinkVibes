4a023eb80a5427d15900c0a8e10abfb92475b78d
Alessandro Zirondelli <alessandr.zirondelli@studio.unibo.it>
2022-04-22 12:39:38 +0200

    changes in registration. Now there isn't user check. It loads only main-registrationform

--- registration.php
+++ registration.php
@@ -8,20 +8,9 @@ $cssArray[0]="./assets/css/login-style.css";
 $jsArray[0]="./assets/js/insertAccount.js";
 $manager = new ManagerAccounts();
 
-if($manager -> isUserLoggedIn()){
-    //pagina admin
-    $templateParams["title"] = "Registration";
-    if($_SESSION["type"] == "Admin") { 
-        $templateParams["main-content"] = "main-registrationform.php";
-    } else {
-        $templateParams["main-content"] = "main-registrationhome.php";
-    }
+$templateParams["title"] = "Registration";
+$templateParams["main-content"] = "main-registrationform.php";
 
-}else{ //Registrazione come utente
-    $templateParams["title"] = "Registration";
-    $templateParams["main-content"] = "main-registrationform.php";
-
-}
 
 require_once ("template/base.php");
 
