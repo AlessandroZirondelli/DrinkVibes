25ef0ce84e3edb463427b6c287d2772608827526
Alessandro Zirondelli <alessandr.zirondelli@studio.unibo.it>
2022-04-23 19:16:28 +0200

    Modify graphics of main-manageaccount.php for admin

--- template/main-manageaccounts.php
+++ template/main-manageaccounts.php
@@ -1,23 +1,24 @@
 
-<div class="main-panel">
-    <div class="page-header">
-        <h2 class="mb-0 text-right mt-4 ">Admin Menu</h2>    
+<div class="container ">
+    <div class="my-3 ">
+        <h1 class="mb-0 text-center mt-4 ">Admin Menu</h1>    
     </div>
-    <div class="row">
-        <div class="col-md-4  grid-margin m-4">
-            <div class="bg-light border p-3">
-            <br></br>
-                <h3 class="mb-5 text-center"><a href="menu-admin.php">See your data</a></h3>
-            </div>
+    <div class="row mb-3">
+        <div class=" col-2 col-md-4">
         </div>
-        
-        <div class="col-md-4  grid-margin m-4">
-            <div class="bg-light border p-3">
-                <br></br>
-                <h3 class="mb-5 text-center"><a href="registration.php">Add new user</a></h3>
-                <h7></h7>
-            </div>
+        <div class=" col-4 col-md-2">
+            <a href="menu-admin.php" class="text-decoration-none">
+                <img class="img-fluid" src="./../DrinkVibes/assets/img/user-info-icon2.png">
+                <figcaption class="text-center text-black">User info</figcaption>
+            </a>
         </div>
-              
+        <div class="col-4 col-md-2">
+            <a href="registration.php" class="text-decoration-none">
+                <img class="img-fluid" src="./../DrinkVibes/assets/img/user-add-icon2.png">  
+                <figcaption class="text-center text-black">Add new user</figcaption>
+            </a>  
+        </div>    
+        <div class="col-2col-md-4">
+        </div>         
     </div>
 </div>
