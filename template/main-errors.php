8fa3ddbf933692ed1e8d2ba24e28df03de5e82b7
Alessandro Zirondelli <alessandr.zirondelli@studio.unibo.it>
2022-05-20 09:52:41 +0200

    Resolve errors infodrinks if parameters aren't set

--- template/main-errors.php
+++ template/main-errors.php
@@ -10,6 +10,6 @@
     <h4 class="alert-heading">Some errors occurs!</h4>
     <p> Error <?php echo $errorNum ?> </p>
     <hr>
-    <p class="mb-0">Please retry again ! Click here</p>
+    <p class="mb-0">Please retry again !</p>
   </div>
 </div>
