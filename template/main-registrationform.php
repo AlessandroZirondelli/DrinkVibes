3ba5888fe80a3ad85ba23eebe465cd056f18272a
Arianna Pagano <arianna.paganostudio.unibo.it>
2022-05-20 12:37:52 +0200

    remove error accessibility in registration

--- template/main-registrationform.php
+++ template/main-registrationform.php
@@ -11,8 +11,8 @@
                 <span class="input-group-text"><i class="fas fa-user"></i></span>
               </div>
               <label class="visually-hidden" for="name">
-              Name
-            </label>
+                Name
+              </label>
               <input name="name" type="text" class="input form-control" id="name" placeholder="Name" />
             </div>
           </div>
@@ -23,8 +23,8 @@
                 <span class="input-group-text"><i class="fas fa-user"></i></span>
               </div>
               <label class="visually-hidden" for="surname">
-              Surname
-            </label>
+                Surname
+              </label>
               <input name="surname" type="text" class="input form-control" id="surname" placeholder="Surname" />
             </div>
           </div>
@@ -35,8 +35,8 @@
                 <span class="input-group-text"><i class="fas fa-user"></i></span>
               </div>
               <label class="visually-hidden" for="userID">
-              Username
-            </label>
+                Username
+              </label>
               <input name="userID" type="text" class="input form-control" id="userID" placeholder="Username" />
             </div>
           </div>
@@ -48,8 +48,8 @@
                 <span class="input-group-text"><i class="fas fa-paper-plane"></i></span>
               </div>
               <label class="visually-hidden" for="email">
-              Email
-            </label>
+                Email
+              </label>
               <input name="email" type="text" class="input form-control" id="email" placeholder="E-mail" />
             </div>
           </div>
@@ -60,8 +60,8 @@
                 <span class="input-group-text"><i class="fas fa-calendar-alt"></i></span>
               </div>
               <label class="visually-hidden" for="birthday">
-              Birthday
-            </label>
+                Birthday
+              </label>
               <input name="birthday" type="text" class="input form-control" id="birthday" placeholder="Birthday (yyyy-mm-dd)" />
             </div>
           </div>
@@ -72,8 +72,8 @@
                 <span class="input-group-text"><i class="fas fa-lock"></i></span>
               </div>
               <label class="visually-hidden" for="password1">
-              Password
-            </label>
+                Password
+              </label>
               <input name="password1" type="password" class="input form-control" id="password1" placeholder="Password" />
             </div>
           </div>
@@ -84,8 +84,8 @@
                 <span class="input-group-text"><i class="fas fa-lock"></i></span>
               </div>
               <label class="visually-hidden" for="password2">
-              Password
-            </label>
+                Password
+              </label>
               <input name="password2" type="password" class="input form-control" id="password2" placeholder="Reapet Password" />
             </div>
           </div>
@@ -99,33 +99,34 @@
                 <div id="tipology" class="mb-3 mt-3">
                   <label>Type:</label>
                   <div class="form-check">
-                    
-                    <input class="form-check-input" type="radio" name="tipology" <?php if (isset($_SESSION["type"]) && $_SESSION["type"] == "Admin"); ?> id="radioButtonAdmin" value="Admin" checked>
+
+                    <input class="form-check-input" type="radio" name="tipology" <?php if (isset($_SESSION["type"]) && $_SESSION["type"] == "Admin"); ?> id="radioButtonAdmin" value="Admin" checked />
                     <label class="form-check-label" for="radioButtonAdmin">
                       Admin
                     </label>
                   </div>
                   <div class="form-check">
-                    <input class="form-check-input" type="radio" name="tipology" <?php if (isset($_SESSION["type"]) && $_SESSION["type"] == "Admin"); ?> id="radioButtonUser" value="User" checked>
+                    <input class="form-check-input" type="radio" name="tipology" <?php if (isset($_SESSION["type"]) && $_SESSION["type"] == "Admin"); ?> id="radioButtonUser" value="User" checked />
                     <label class="form-check-label" for="radioButtonUser">
                       User
                     </label>
                   </div>
                   <div class="form-check">
-                    <input class="form-check-input" type="radio" name="tipology" <?php if (isset($_SESSION["type"]) && $_SESSION["type"] == "Admin"); ?> id="radioButtonExpress" value="Express" checked>
+                    <input class="form-check-input" type="radio" name="tipology" <?php if (isset($_SESSION["type"]) && $_SESSION["type"] == "Admin"); ?> id="radioButtonExpress" value="Express" checked />
                     <label class="form-check-label" for="radioButtonExpress">
                       Express
                     </label>
                   </div>
                 </div>
               </div>
+            </div>
             <?php endif; ?>
 
             <div class="col-12 text-right">
               <input type="submit" name="insertButton" value="Submit" class="btn btn-dark text-uppercase mb-3 mt-3" />
             </div>
             <div id="warningsLabel"></div>
-            </div>
+        
         </div>
       </fieldset>
     </form>
