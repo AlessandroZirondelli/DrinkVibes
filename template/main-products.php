ee6ef208643ec1b5e69645c7048a7b5b5ac11355
Arianna Pagano <arianna.paganostudio.unibo.it>
2022-05-20 09:55:16 +0200

    modify dimension card in products and products admin

--- template/main-products.php
+++ template/main-products.php
@@ -1,5 +1,5 @@
 <h1 class="p-3">Shop Products</h1>
-<div class="container mb-5 p-0">
+<div class="container mb-5 p-6">
 
     <div class="row">
         <?php foreach ($templateParams["All"] as $products) : ?>
